f9427443e03ca102ca34404a83b0f032fc99b9e3
misahh-wsb <gdx153742@student.wsb.gda.pl>
2022-01-30 16:36:29 +0100

    Zad1

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,33 @@
 public class Animal {
+    final String species = "";
+    private Double weight;
+    private boolean life;
+
+    public Animal(){
+        weight=5.0;
+        life=true;
+    }
+    void feed()
+    {
+        if(!life){
+        System.out.println("Zwierze nie żyje");
+        return;
+    }
+        weight+=0.5;
+    }
+    void takeForAWalk()
+    {
+        if(!life){
+            System.out.println("Zwierze nie żyje");
+            return;
+        }
+
+        weight-=0.5;
+
+        if(weight<=0)
+        {
+            System.out.println("Zwierzę zmarło");
+            life=false;
+        }
+    }
 }
--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,6 @@
 public class Human {
+
+    Animal pet;
+
+
 }
